0ed803557e2da8fc7c51f6b819d4f1627557935f
Brittany Fisher <bribreana7@gmail.com>
2021-06-22 17:12:59 -0400

    initial commit

---
+++ sql/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE rankings (
+    id serial primary key,
+    ranking_title text NOT NULL
+);
+
+CREATE TABLE topics (
+    id serial primary key,
+    topic_name text NOT NULL,
+    topic_score integer REFRENCES rankings(id)
+);
---
+++ sql/seed.sql
@@ -0,0 +1,19 @@
+INSERT INTO rankings
+    (rankings)
+VALUES
+('html'),
+('css'),
+('javascript'),
+('postgressql'),
+('node'),
+('express');
+
+INSERT INTO topics
+    (topic_name, topic_score)
+VALUES
+    ('awesome', 5),
+    ('great', 4),
+    ('good', 3),
+    ('okay', 2),
+    ('poor', 1),
+    ('unranked', 0);
